ff94938cb76814c99282756696826ca21da49144
cjwoodru2 <cjwoodru2@gmail.com>
2017-02-11 12:29:05 -0500

    Comlete a mock auth app to demonstrate HOC's and wrapping components in them

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Special Sauce
+      <ul>
+        <li>Hard Work</li>
+        <li>Determination</li>
+        <li>Passion</li>
+      </ul>
+    </div>
+  )
+}
